bc001a785d4f6e7cf5b21af1916a9d21610681e3
Dima <dimabersenev.13@yandex.ru>
2025-10-08 14:56:30 +0500

    added ESLint rules from Digital Element

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,8 @@
-import './styles/global.pcss'
-import FormManager from './js/form'
-import Modal from './js/modal.js'
+import "./styles/global.pcss";
+import FormManager from "./js/form";
+import Modal from "./js/modal.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-	const form = new FormManager()
-	const modal = new Modal('[data-js-modal=modal1]')
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const form = new FormManager();
+  const modal = new Modal("[data-js-modal=modal1]");
+});
--- frontend/src/js/utils/bodyUnLock.js
+++ frontend/src/js/utils/bodyUnLock.js
@@ -1,11 +1,14 @@
+/**
+ *
+ */
 export const BodyUnLock = (unlock, timeout) => {
-	setTimeout(() => {
-		document.body.style.paddingRight = '0px'
-		document.body.style.overflow = 'visible'
-	}, timeout)
+  setTimeout(() => {
+    document.body.style.paddingRight = "0px";
+    document.body.style.overflow = "visible";
+  }, timeout);
 
-	unlock = false
-	setTimeout(() => {
-		unlock = true
-	}, timeout)
-}
+  unlock = false;
+  setTimeout(() => {
+    unlock = true;
+  }, timeout);
+};
